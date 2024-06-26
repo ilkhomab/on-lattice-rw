ceea1e4cd649b554e7d9989cdf34448387bcd592
Ilkhom Abdurakhmanov <ilkhom@pawsey.org.au>
2024-06-26 14:57:01 +0800

    finds points and neighbours correctly, box array needs to be introduced

--- fortran_implementation/main.f90
+++ fortran_implementation/main.f90
@@ -63,8 +63,14 @@ program main
     integer :: number_of_fresh_points, number_of_valid_neighbours
     integer :: tmp_hash 
     integer :: index
-    integer :: i0, j, j0, k, k0, h
-
+    integer :: i0, i, j, k, h, h0
+!
+    ! ANSI escape code for red color
+    character(len=*), parameter :: red = char(27) // '[31m'
+    character(len=*), parameter :: green = char(27) // '[32m'
+    character(len=*), parameter :: blue = char(27) // '[34m'
+    ! ANSI escape code to reset color
+    character(len=*), parameter :: reset = char(27) // '[0m'
 
     max_points=2147483647  ! 2^31 - 1, the maximum int4 range
 
@@ -81,78 +87,75 @@ program main
     fresh_point(1)%y=0
     fresh_point(1)%z=0
 
-
     number_of_fresh_points=1
-
+    !add the fresh point to the box
+    !box(number_of_fresh_points)=fresh_point(1)
+    !add a hash of the fresh point to the array of hashes
+    hash(number_of_fresh_points)=0
     i0=0
     do while(i0 <= max_points .and. number_of_fresh_points /= 0)
-      
-      j0=0
-      print*,' number_of_fresh_points=', number_of_fresh_points
+      number_of_valid_neighbours=0
       do j = 1, number_of_fresh_points
-
-        number_of_valid_neighbours=0
-        k0=0
+        write(*, '(A, A, 3I2, A)') red, ' FRESH POINT=', fresh_point(j)%x, fresh_point(j)%y, fresh_point(j)%z, reset
         do k = 1, 6  ! run through the 6 neigbours and check if they are valid neighbours
           neighbours(k)%x = fresh_point(j)%x+cube_lattice_perturbations(k)%x 
           neighbours(k)%y = fresh_point(j)%y+cube_lattice_perturbations(k)%y 
           neighbours(k)%z = fresh_point(j)%z+cube_lattice_perturbations(k)%z 
-
           ! check if the neighbour points are in the domain
           if ((neighbours(k)%x >= 0 .and. neighbours(k)%x < xmax) .and. &
-            &(neighbours(k)%y >= 0 .and. neighbours(k)%y < ymax) .and. &
-            &(neighbours(k)%z >= 0 .and. neighbours(k)%z < zmax)) then
-
-
-            fresh_point(j)%neighbour(k)%x = neighbours(k)%x
-            fresh_point(j)%neighbour(k)%y = neighbours(k)%y
-            fresh_point(j)%neighbour(k)%z = neighbours(k)%z
-
+             &(neighbours(k)%y >= 0 .and. neighbours(k)%y < ymax) .and. &
+             &(neighbours(k)%z >= 0 .and. neighbours(k)%z < zmax)) then
             number_of_valid_neighbours=number_of_valid_neighbours+1
-
-            future_points(j0+k0+1)=neighbours(k)
-            future_points_hashes(j0+k0+1)=get_hash(future_points(j0+k)%x,&
-                                               &future_points(j0+k)%y,&
-                                               &future_points(j0+k)%z)
-            k0=k0+1
-
+            future_points(number_of_valid_neighbours)=neighbours(k)
+            future_points_hashes(number_of_valid_neighbours)=get_hash(future_points(number_of_valid_neighbours)%x,&
+                                                                     &future_points(number_of_valid_neighbours)%y,&
+                                                                     &future_points(number_of_valid_neighbours)%z)
           endif
-          
-
         enddo ! do k = 1, 6
-
-        !add the fresh point to the box
-        box(i0+j) = fresh_point(j)
-        !add a hash of the fresh point to the array of hashes
-        hash(i0+j) = get_hash(fresh_point(j)%x,&
-                             &fresh_point(j)%y,&
-                             &fresh_point(j)%z)
-        j0=j0+number_of_valid_neighbours
-
-
       enddo ! do j = 1, number_of_fresh_points
-
-      i0 = i0 + number_of_fresh_points
-
       number_of_fresh_points=0
-      do h=1,j0
+      do h=1,number_of_valid_neighbours
 ! the following if condition checks if future_points_hashes(h) is not in the hash array
         if (any(future_points_hashes(h) == hash(1:i0))) then
 ! this is not a fresh point
           cycle
         else
-! this is a fresh point
-          fresh_point(h)%x=future_points(h)%x  
-          fresh_point(h)%y=future_points(h)%y  
-          fresh_point(h)%z=future_points(h)%z  
           number_of_fresh_points=number_of_fresh_points+1
+          i0=i0+number_of_fresh_points
+! this is a fresh point
+          fresh_point(number_of_fresh_points)%x=future_points(h)%x  
+          fresh_point(number_of_fresh_points)%y=future_points(h)%y  
+          fresh_point(number_of_fresh_points)%z=future_points(h)%z  
+          hash(i0) = future_points_hashes(h)
+          write(*,'(A, A, I10, A, 3I2, A)')green, 'HASH:', future_points_hashes(h), '  fresh point', & 
+                                       fresh_point(number_of_fresh_points)%x, &
+                                       fresh_point(number_of_fresh_points)%y, &
+                                       fresh_point(number_of_fresh_points)%z, reset
         endif
-        
       enddo
+    enddo ! while(i0 <= max_points)
+
+!            box(i0+j)%neighbour(number_of_valid_neighbours)%x=neighbours(k)%x
+!            box(i0+j)%neighbour(number_of_valid_neighbours)%y=neighbours(k)%y
+!            box(i0+j)%neighbour(number_of_valid_neighbours)%z=neighbours(k)%z
+!          box(i0)%x = fresh_point(j)%x 
+!          box(i0)%y = fresh_point(j)%y 
+!          box(i0)%z = fresh_point(j)%z 
 
       ! Pause execution for 5 seconds
-      CALL SYSTEM("sleep 5")
-    enddo ! while(i0 <= max_points)
+      !CALL SYSTEM("sleep 5")
+do i=1,24
+write(*, '(A, I10,A, A, I1, A, I1, A, I1, A, 6(A, I1, A, I1, A, I1), A)') &
+    'hash:', hash(i), ', ', &
+    'point: (', box(i)%x, ',', box(i)%y, ',', box(i)%z, '), neighbours:', '(', &
+    box(i)%neighbour(1)%x, ',', box(i)%neighbour(1)%y, ',', box(i)%neighbour(1)%z, '), (', &
+    box(i)%neighbour(2)%x, ',', box(i)%neighbour(2)%y, ',', box(i)%neighbour(2)%z, '), (', &
+    box(i)%neighbour(3)%x, ',', box(i)%neighbour(3)%y, ',', box(i)%neighbour(3)%z, '), (', &
+    box(i)%neighbour(4)%x, ',', box(i)%neighbour(4)%y, ',', box(i)%neighbour(4)%z, '), (', &
+    box(i)%neighbour(5)%x, ',', box(i)%neighbour(5)%y, ',', box(i)%neighbour(5)%z, '), (', &
+    box(i)%neighbour(6)%x, ',', box(i)%neighbour(6)%y, ',', box(i)%neighbour(6)%z, ')'
+enddo
+
 
 !    point(1)%neighbour(1)%x = 2 
 !    point(1)%neighbour(1)%y = 0 
